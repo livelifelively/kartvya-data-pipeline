358387ec235b62f779cea0f72b6a96587488e650
ashish rana <dev.livelifelively@gmail.com>
2024-12-09 18:46:25 +0530

    update node

--- api/js-ts/src/knowledge-graph/generic/generic.create.ts
+++ api/js-ts/src/knowledge-graph/generic/generic.create.ts
@@ -18,6 +18,29 @@ export async function createNodeType(nodetype: string, graphQLClient: any, nodeD
   // return response;
 }
 
+export async function updateNodeType(
+  nodetype: string,
+  graphQLClient: any,
+  input: { filter?: any; set?: any; remove?: any }
+) {
+  const mutation = `
+        mutation Update${nodetype}($input: Update${nodetype}Input!) {
+            update${nodetype}(input: $input) {
+                ${nodetype} {
+                    id
+                }
+            }
+        }
+        `;
+
+  const variables = {
+    input,
+  };
+
+  const response = await graphQLClient.request(mutation, variables);
+  return response[`update${nodetype}`][nodetype][0].id;
+}
+
 // export async function upsertNodeType(nodetype: string, graphQLClient: any, nodeData: any) {
 //   const query = `
 //     query Query${nodetype}
