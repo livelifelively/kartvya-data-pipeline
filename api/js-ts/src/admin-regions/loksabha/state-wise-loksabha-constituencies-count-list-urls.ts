5a72eafddf7c0882be01123d1d8af8c886092ec6
ashish rana <dev.livelifelively@gmail.com>
2024-12-19 21:07:52 +0530

    lists fixes

---
+++ api/js-ts/src/admin-regions/loksabha/state-wise-loksabha-constituencies-count-list-urls.ts
@@ -0,0 +1,182 @@
+export const stateLoksabhaConstituenciesCountList = [
+  {
+    state: "andaman and nicobar islands",
+    loksabhaConstituenciesCount: 1,
+    stateNameId: "in-sut-andaman-nicobar-islands",
+  },
+  {
+    state: "andhra pradesh",
+    loksabhaConstituenciesCount: 25,
+    stateNameId: "in-sut-andhra-pradesh",
+  },
+  {
+    state: "arunachal pradesh",
+    loksabhaConstituenciesCount: 2,
+    stateNameId: "in-sut-arunachal-pradesh",
+  },
+  {
+    state: "assam",
+    loksabhaConstituenciesCount: 14,
+    stateNameId: "in-sut-assam",
+  },
+  {
+    state: "bihar",
+    loksabhaConstituenciesCount: 40,
+    stateNameId: "in-sut-bihar",
+  },
+  {
+    state: "chandigarh",
+    loksabhaConstituenciesCount: 1,
+    stateNameId: "in-sut-chandigarh",
+  },
+  {
+    state: "chhattisgarh",
+    loksabhaConstituenciesCount: 11,
+    stateNameId: "in-sut-chhattisgarh",
+  },
+  {
+    state: "dadra and nagar haveli and daman and diu",
+    loksabhaConstituenciesCount: 2,
+    stateNameId: "in-sut-dadra-nagar-haveli-daman-diu",
+  },
+  {
+    state: "delhi",
+    loksabhaConstituenciesCount: 7,
+    stateNameId: "in-sut-delhi",
+  },
+  {
+    state: "goa",
+    loksabhaConstituenciesCount: 2,
+    stateNameId: "in-sut-goa",
+  },
+  {
+    state: "gujarat",
+    loksabhaConstituenciesCount: 26,
+    stateNameId: "in-sut-gujarat",
+  },
+  {
+    state: "haryana",
+    loksabhaConstituenciesCount: 10,
+    stateNameId: "in-sut-haryana",
+  },
+  {
+    state: "himachal pradesh",
+    loksabhaConstituenciesCount: 4,
+    stateNameId: "in-sut-himachal-pradesh",
+  },
+  {
+    state: "jammu and kashmir",
+    loksabhaConstituenciesCount: 5,
+    stateNameId: "in-sut-jammu-kashmir",
+  },
+  {
+    state: "jharkhand",
+    loksabhaConstituenciesCount: 14,
+    stateNameId: "in-sut-jharkhand",
+  },
+  {
+    state: "karnataka",
+    loksabhaConstituenciesCount: 28,
+    stateNameId: "in-sut-karnataka",
+  },
+  {
+    state: "kerala",
+    loksabhaConstituenciesCount: 20,
+    stateNameId: "in-sut-kerala",
+  },
+  {
+    state: "ladakh",
+    loksabhaConstituenciesCount: 1,
+    stateNameId: "in-sut-ladakh",
+  },
+  {
+    state: "lakshadweep",
+    loksabhaConstituenciesCount: 1,
+    stateNameId: "in-sut-lakshadweep",
+  },
+  {
+    state: "madhya pradesh",
+    loksabhaConstituenciesCount: 29,
+    stateNameId: "in-sut-madhya-pradesh",
+  },
+  {
+    state: "maharashtra",
+    loksabhaConstituenciesCount: 48,
+    stateNameId: "in-sut-maharashtra",
+  },
+  {
+    state: "manipur",
+    loksabhaConstituenciesCount: 2,
+    stateNameId: "in-sut-manipur",
+  },
+  {
+    state: "meghalaya",
+    loksabhaConstituenciesCount: 2,
+    stateNameId: "in-sut-meghalaya",
+  },
+  {
+    state: "mizoram",
+    loksabhaConstituenciesCount: 1,
+    stateNameId: "in-sut-mizoram",
+  },
+  {
+    state: "nagaland",
+    loksabhaConstituenciesCount: 1,
+    stateNameId: "in-sut-nagaland",
+  },
+  {
+    state: "odisha",
+    loksabhaConstituenciesCount: 21,
+    stateNameId: "in-sut-odisha",
+  },
+  {
+    state: "puducherry",
+    loksabhaConstituenciesCount: 1,
+    stateNameId: "in-sut-puducherry",
+  },
+  {
+    state: "punjab",
+    loksabhaConstituenciesCount: 13,
+    stateNameId: "in-sut-punjab",
+  },
+  {
+    state: "rajasthan",
+    loksabhaConstituenciesCount: 25,
+    stateNameId: "in-sut-rajasthan",
+  },
+  {
+    state: "sikkim",
+    loksabhaConstituenciesCount: 1,
+    stateNameId: "in-sut-sikkim",
+  },
+  {
+    state: "tamil nadu",
+    loksabhaConstituenciesCount: 39,
+    stateNameId: "in-sut-tamil-nadu",
+  },
+  {
+    state: "telangana",
+    loksabhaConstituenciesCount: 17,
+    stateNameId: "in-sut-telangana",
+  },
+  {
+    state: "tripura",
+    loksabhaConstituenciesCount: 2,
+    stateNameId: "in-sut-tripura",
+  },
+  {
+    state: "uttar pradesh",
+    loksabhaConstituenciesCount: 80,
+    stateNameId: "in-sut-uttar-pradesh",
+  },
+  {
+    state: "uttarakhand",
+    loksabhaConstituenciesCount: 5,
+    stateNameId: "in-sut-uttarakhand",
+  },
+  {
+    state: "west bengal",
+    loksabhaConstituenciesCount: 42,
+    stateNameId: "in-sut-west-bengal",
+  },
+];
