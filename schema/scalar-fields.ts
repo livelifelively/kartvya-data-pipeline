58afbd49b90594303ad4dc07dd2fde6d5f1b50fc
ashish rana <dev.livelifelively@gmail.com>
2025-01-05 15:21:44 +0530

    single to double quotes

--- schema/scalar-fields.ts
+++ schema/scalar-fields.ts
@@ -1,157 +1,157 @@
-export const LANGUAGE__SCALARS = ['id', 'name_en', 'names', 'wikidata_qid', 'wikipedia_page'];
-export const SCRIPT__SCALARS = ['id', 'name_en', 'names', 'wikidata_qid', 'wikipedia_page'];
-export const LANGUAGE_SCRIPT__SCALARS = ['id', 'name_en'];
+export const LANGUAGE__SCALARS = ["id", "name_en", "names", "wikidata_qid", "wikipedia_page"];
+export const SCRIPT__SCALARS = ["id", "name_en", "names", "wikidata_qid", "wikipedia_page"];
+export const LANGUAGE_SCRIPT__SCALARS = ["id", "name_en"];
 
-export const REGION_NAME__SCALARS = ['id', 'name'];
+export const REGION_NAME__SCALARS = ["id", "name"];
 export const ADMINISTRATIVE_ENTITY__SCALARS = [
-  'id',
-  'category',
-  'wikidata_qid',
-  'wikipedia_page',
-  'established_on',
-  'dissolved_on',
+  "id",
+  "category",
+  "wikidata_qid",
+  "wikipedia_page",
+  "established_on",
+  "dissolved_on",
 ];
 
 export const NATION_REGION__SCALARS = [
-  'id',
-  'region_id',
-  'wikidata_qid',
-  'wikipedia_page',
-  'established_on',
-  'dissolved_on',
+  "id",
+  "region_id",
+  "wikidata_qid",
+  "wikipedia_page",
+  "established_on",
+  "dissolved_on",
 ];
-export const NATION_LEGAL__SCALARS = ['id', 'wikidata_qid', 'wikipedia_page', 'established_on', 'dissolved_on'];
+export const NATION_LEGAL__SCALARS = ["id", "wikidata_qid", "wikipedia_page", "established_on", "dissolved_on"];
 
 export const ZONAL_COUNCIL_REGION__SCALARS = [
-  'id',
-  'region_id',
-  'wikidata_qid',
-  'wikipedia_page',
-  'established_on',
-  'dissolved_on',
+  "id",
+  "region_id",
+  "wikidata_qid",
+  "wikipedia_page",
+  "established_on",
+  "dissolved_on",
 ];
-export const ZONAL_COUNCIL_LEGAL__SCALARS = ['id', 'established_on', 'dissolved_on', 'wikidata_qid', 'wikipedia_page'];
+export const ZONAL_COUNCIL_LEGAL__SCALARS = ["id", "established_on", "dissolved_on", "wikidata_qid", "wikipedia_page"];
 
 export const STATE_REGION__SCALARS = [
-  'id',
-  'region_id',
-  'wikidata_qid',
-  'wikipedia_page',
-  'established_on',
-  'dissolved_on',
+  "id",
+  "region_id",
+  "wikidata_qid",
+  "wikipedia_page",
+  "established_on",
+  "dissolved_on",
 ];
 export const STATE_LEGAL__SCALARS = [
-  'id',
-  'wikidata_qid',
-  'wikipedia_page',
-  'established_on',
-  'dissolved_on',
-  'iso_code',
-  'vehicle_code',
-  'official_website',
+  "id",
+  "wikidata_qid",
+  "wikipedia_page",
+  "established_on",
+  "dissolved_on",
+  "iso_code",
+  "vehicle_code",
+  "official_website",
 ];
 
 export const UNION_TERRITORY_REGION__SCALARS = [
-  'id',
-  'region_id',
-  'wikidata_qid',
-  'wikipedia_page',
-  'established_on',
-  'dissolved_on',
+  "id",
+  "region_id",
+  "wikidata_qid",
+  "wikipedia_page",
+  "established_on",
+  "dissolved_on",
 ];
 export const UNION_TERRITORY_LEGAL__SCALARS = [
-  'id',
-  'wikidata_qid',
-  'wikipedia_page',
-  'established_on',
-  'dissolved_on',
-  'iso_code',
-  'vehicle_code',
-  'official_website',
+  "id",
+  "wikidata_qid",
+  "wikipedia_page",
+  "established_on",
+  "dissolved_on",
+  "iso_code",
+  "vehicle_code",
+  "official_website",
 ];
 
 export const DISTRICT_REGION__SCALARS = [
-  'id',
-  'region_id',
-  'wikidata_qid',
-  'wikipedia_page',
-  'established_on',
-  'dissolved_on',
+  "id",
+  "region_id",
+  "wikidata_qid",
+  "wikipedia_page",
+  "established_on",
+  "dissolved_on",
 ];
 export const DISTRICT_LEGAL__SCALARS = [
-  'id',
-  'wikidata_qid',
-  'wikipedia_page',
-  'established_on',
-  'dissolved_on',
-  'official_website',
+  "id",
+  "wikidata_qid",
+  "wikipedia_page",
+  "established_on",
+  "dissolved_on",
+  "official_website",
 ];
 
 export const CITY_REGION__SCALARS = [
-  'id',
-  'region_id',
-  'wikidata_qid',
-  'wikipedia_page',
-  'established_on',
-  'dissolved_on',
+  "id",
+  "region_id",
+  "wikidata_qid",
+  "wikipedia_page",
+  "established_on",
+  "dissolved_on",
 ];
-export const CITY_LEGAL__SCALARS = ['id', 'wikidata_qid', 'wikipedia_page', 'established_on', 'dissolved_on'];
+export const CITY_LEGAL__SCALARS = ["id", "wikidata_qid", "wikipedia_page", "established_on", "dissolved_on"];
 
 export const LOKSABHA_CONSTITUENCY_REGION__SCALARS = [
-  'id',
-  'region_id',
-  'wikidata_qid',
-  'wikipedia_page',
-  'established_on',
-  'dissolved_on',
+  "id",
+  "region_id",
+  "wikidata_qid",
+  "wikipedia_page",
+  "established_on",
+  "dissolved_on",
 ];
 export const LOKSABHA_CONSTITUENCY_LEGAL__SCALARS = [
-  'id',
-  'wikidata_qid',
-  'wikipedia_page',
-  'established_on',
-  'dissolved_on',
+  "id",
+  "wikidata_qid",
+  "wikipedia_page",
+  "established_on",
+  "dissolved_on",
 ];
 
 export const VIDHANSABHA_CONSTITUENCY_REGION__SCALARS = [
-  'id',
-  'region_id',
-  'wikidata_qid',
-  'wikipedia_page',
-  'established_on',
-  'dissolved_on',
+  "id",
+  "region_id",
+  "wikidata_qid",
+  "wikipedia_page",
+  "established_on",
+  "dissolved_on",
 ];
 export const VIDHANSABHA_CONSTITUENCY_LEGAL__SCALARS = [
-  'id',
-  'wikidata_qid',
-  'wikipedia_page',
-  'established_on',
-  'dissolved_on',
+  "id",
+  "wikidata_qid",
+  "wikipedia_page",
+  "established_on",
+  "dissolved_on",
 ];
 
 export const NATIONAL_CAPITAL_CITY__SCALARS = [
-  'id',
-  'wikidata_qid',
-  'wikipedia_page',
-  'established_on',
-  'dissolved_on',
-  'category',
+  "id",
+  "wikidata_qid",
+  "wikipedia_page",
+  "established_on",
+  "dissolved_on",
+  "category",
 ];
 export const UNION_TERRITORY_CAPITAL_CITY__SCALARS = [
-  'id',
-  'wikidata_qid',
-  'wikipedia_page',
-  'established_on',
-  'dissolved_on',
-  'category',
+  "id",
+  "wikidata_qid",
+  "wikipedia_page",
+  "established_on",
+  "dissolved_on",
+  "category",
 ];
 export const STATE_CAPITAL_CITY__SCALARS = [
-  'id',
-  'wikidata_qid',
-  'wikipedia_page',
-  'established_on',
-  'dissolved_on',
-  'category',
+  "id",
+  "wikidata_qid",
+  "wikipedia_page",
+  "established_on",
+  "dissolved_on",
+  "category",
 ];
 
 export const ALL_SCALARS: { [key: string]: string[] } = {
