b51fda70d45cc95f2f9a6bc819fd6f270d72725e
ashish rana <dev.livelifelively@gmail.com>
2025-01-25 05:22:19 +0530

    manipur districts

---
+++ api/js-ts/src/kg-pipelines/d-v-l/manipur/scripts/districts.ts
@@ -0,0 +1,204 @@
+import path from "path";
+import {
+  addDistrictDataToKnowledgeGraph,
+  fetchDistrictSOIGeoFeatures,
+  fetchDistrictsOSMDetails,
+  fetchDistrictsOSMRelationIds,
+  fetchDistrictsWikiDetails,
+  fetchStateDistricts,
+  fetchStateOSMData,
+  transformDistrictsWikipediaData,
+  transformDistrictsWithOSM,
+  transformDistrictsWithSOIGeo,
+} from "../../../../pipeline/districts-pipeline";
+import { PipelineStep, runPipeline } from "../../../../pipeline/pipeline";
+
+async function districtsPipeline(stateUT: any, districtsList: any, saveToKG: boolean = false) {
+  console.log("DISTRICTS PROCESSING INITIALIZED: ", stateUT.name);
+
+  const steps: PipelineStep[] = [
+    {
+      name: "Fetch State Districts",
+      function: fetchStateDistricts,
+      key: "STATE_DISTRICTS_LIST",
+      input: null,
+    },
+    {
+      name: "Fetch State OSM Data",
+      function: fetchStateOSMData,
+      key: "STATE_OSM_DATA",
+      input: null,
+    },
+    {
+      name: "Fetch Districts OSM Relation IDs",
+      function: fetchDistrictsOSMRelationIds,
+      key: "STATE_DISTRICTS_OSM_RELATION_IDS",
+      input: null, // Will be set after the second step
+    },
+    {
+      name: "Fetch Districts OSM Details",
+      function: fetchDistrictsOSMDetails,
+      key: "STATE_DISTRICTS_OSM_RELATION_IDS",
+
+      input: null, // Will be set after the third step
+    },
+    {
+      name: "Fetch Districts Wiki Details",
+      function: fetchDistrictsWikiDetails,
+      key: "STATE_DISTRICTS_WIKI_DATA",
+      input: null, // Will be set after the first step
+    },
+    // {
+    //   name: "Fetch District SOI Geo Features",
+    //   function: fetchDistrictSOIGeoFeatures,
+    //   key: "STATE_DISTRICTS_SOI_GEO_DATA",
+    //   input: null,
+    // },
+    {
+      name: "Append Wikipedia Data",
+      function: transformDistrictsWikipediaData,
+      input: null, // Will be set after the fifth step
+      key: "APPEND_WIKIPEDIA_DATA_TRANSFORM_STATE_DISTRICTS_DATA",
+    },
+    {
+      name: "Transform Districts with OSM",
+      function: transformDistrictsWithOSM,
+      input: null, // Will be set after the fourth and seventh steps
+      key: "APPEND_OSM_DATA_TRANSFORM_STATE_DISTRICTS_DATA",
+    },
+    // SOI district boundaries are not always to the point as per present situation
+    // {
+    //   name: "Transform Districts with SOI Geo",
+    //   function: transformDistrictsWithSOIGeo,
+    //   input: null, // Will be set after the sixth and seventh steps
+    //   key: "APPEND_SOI_DATA_TRANSFORM_STATE_DISTRICTS_DATA",
+    // },
+  ];
+
+  let outputs: Record<string, any> = {
+    stateUT,
+    districtsList,
+    districtsCount: 0,
+    districts: [],
+    state_union_territory_id: "",
+    state_union_territory_osm_id: "",
+    state_osm_data: {},
+    districtsRelationIds: {},
+    osmDistrictsCount: 0,
+    districtsOSMDetails: [],
+    districtsOSMDetailsNotFound: [],
+    osmDetailsDistrictsCount: 0,
+    districtsWikiDetails: [],
+    districtsWikiDetailsFailed: [],
+    districtFeaturesSOI: [],
+    transformedDistrictsWikipedia: [],
+    districtsNotTransformedWikipedia: [],
+    fullMatchDistrictsOSMWiki: [],
+    partialMatchDistrictsOSMWiki: [],
+    allMatchedDistrictsOSMWiki: [],
+    matchDistrictsOSMWikiStatistics: {},
+    unmatchedDistrictsOSMWiki: [],
+    transformedDistrictsSOIGeo: [],
+    unmatchedDistrictsSOIGeo: [],
+  };
+
+  if (saveToKG) {
+    steps.push({
+      name: "Save Districts to KnowledgeGraph",
+      function: addDistrictDataToKnowledgeGraph,
+      input: null,
+      key: "SAVE_DISTRICT_DATA_TO_KNOWLEDGE_GRAPH",
+    });
+  }
+
+  const districtsProgressDir = path.join(__dirname, "../", "district-pipeline-logs");
+  const progressStatusFile = path.join(districtsProgressDir, "progressStatus.json");
+
+  try {
+    const lastStepOutput = await runPipeline(steps, outputs, districtsProgressDir, progressStatusFile);
+
+    if (saveToKG) return lastStepOutput.savedToKnowledgeGraph;
+    return lastStepOutput.transformedDistrictsSOIGeo;
+  } catch (error) {
+    console.error("Error in processing: ", error);
+  }
+}
+
+(async () => {
+  const stateUT = {
+    name: "manipur",
+    name_id: "in-sut-manipur",
+    vehicle_code: "MN",
+  };
+
+  const districtsList = [
+    {
+      names: ["Bishnupur"],
+      wikipedia_page: "https://en.wikipedia.org/wiki/Bishnupur_District",
+    },
+    {
+      names: ["Chandel"],
+      wikipedia_page: "https://en.wikipedia.org/wiki/Chandel_District",
+    },
+    {
+      names: ["Churachandpur"],
+      wikipedia_page: "https://en.wikipedia.org/wiki/Churachandpur_district",
+    },
+    {
+      names: ["Imphal East"],
+      wikipedia_page: "https://en.wikipedia.org/wiki/Imphal_East",
+    },
+    {
+      names: ["Imphal West"],
+      wikipedia_page: "https://en.wikipedia.org/wiki/Imphal_West",
+    },
+    {
+      names: ["Jiribam"],
+      wikipedia_page: "https://en.wikipedia.org/wiki/Jiribam_District",
+    },
+    {
+      names: ["Kakching"],
+      wikipedia_page: "https://en.wikipedia.org/wiki/Kakching_district",
+    },
+    {
+      names: ["Kamjong"],
+      wikipedia_page: "https://en.wikipedia.org/wiki/Kamjong_district",
+    },
+    {
+      names: ["Kangpokpi"],
+      wikipedia_page: "https://en.wikipedia.org/wiki/Kangpokpi_district",
+    },
+    {
+      names: ["Noney"],
+      wikipedia_page: "https://en.wikipedia.org/wiki/Noney_district",
+    },
+    {
+      names: ["Pherzawl"],
+      wikipedia_page: "https://en.wikipedia.org/wiki/Pherzawl_District",
+    },
+    {
+      names: ["Senapati"],
+      wikipedia_page: "https://en.wikipedia.org/wiki/Senapati_district",
+    },
+    {
+      names: ["Tamenglong"],
+      wikipedia_page: "https://en.wikipedia.org/wiki/Tamenglong_District",
+    },
+    {
+      names: ["Tengnoupal"],
+      wikipedia_page: "https://en.wikipedia.org/wiki/Tengnoupal_district",
+    },
+    {
+      names: ["Thoubal"],
+      wikipedia_page: "https://en.wikipedia.org/wiki/Thoubal_District",
+    },
+    {
+      names: ["Ukhrul"],
+      wikipedia_page: "https://en.wikipedia.org/wiki/Ukhrul_District",
+    },
+  ];
+
+  const saveToKG = true;
+
+  await districtsPipeline(stateUT, districtsList, saveToKG);
+})();
