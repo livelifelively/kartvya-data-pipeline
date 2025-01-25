f2091980b8095b5edb107ddc87e0507af49fe072
ashish rana <dev.livelifelively@gmail.com>
2025-01-25 07:25:40 +0530

    tripura districts

---
+++ api/js-ts/src/kg-pipelines/d-v-l/tripura/scripts/districts.ts
@@ -0,0 +1,179 @@
+// TODO FIXME
+/**
+ * Administratively Puducherry only has 2 districts, Puducherry and Karaikal.
+ * The districts of Yanam and Mahé are census districts and not administrative districts.
+ * The sub-taluks covered by these 2 census districts are administratively under the Puducherry district.[1][2]
+ */
+
+import path from "path";
+import {
+  addDistrictDataToKnowledgeGraph,
+  fetchDistrictSOIGeoFeatures,
+  fetchDistrictsOSMDetails,
+  fetchDistrictsOSMRelationIds,
+  fetchDistrictsWikiDetails,
+  fetchStateDistricts,
+  fetchStateOSMData,
+  transformDistrictsWikipediaData,
+  transformDistrictsWithOSM,
+  transformDistrictsWithSOIGeo,
+} from "../../../../pipeline/districts-pipeline";
+import { PipelineStep, runPipeline } from "../../../../pipeline/pipeline";
+
+async function districtsPipeline(stateUT: any, districtsList: any, saveToKG: boolean = false) {
+  console.log("DISTRICTS PROCESSING INITIALIZED: ", stateUT.name);
+
+  const steps: PipelineStep[] = [
+    {
+      name: "Fetch State Districts",
+      function: fetchStateDistricts,
+      key: "STATE_DISTRICTS_LIST",
+      input: null,
+    },
+    {
+      name: "Fetch State OSM Data",
+      function: fetchStateOSMData,
+      key: "STATE_OSM_DATA",
+      input: null,
+    },
+    {
+      name: "Fetch Districts OSM Relation IDs",
+      function: fetchDistrictsOSMRelationIds,
+      key: "STATE_DISTRICTS_OSM_RELATION_IDS",
+      input: null, // Will be set after the second step
+    },
+    {
+      name: "Fetch Districts OSM Details",
+      function: fetchDistrictsOSMDetails,
+      key: "STATE_DISTRICTS_OSM_RELATION_IDS",
+
+      input: null, // Will be set after the third step
+    },
+    {
+      name: "Fetch Districts Wiki Details",
+      function: fetchDistrictsWikiDetails,
+      key: "STATE_DISTRICTS_WIKI_DATA",
+      input: null, // Will be set after the first step
+    },
+    // {
+    //   name: "Fetch District SOI Geo Features",
+    //   function: fetchDistrictSOIGeoFeatures,
+    //   key: "STATE_DISTRICTS_SOI_GEO_DATA",
+    //   input: null,
+    // },
+    {
+      name: "Append Wikipedia Data",
+      function: transformDistrictsWikipediaData,
+      input: null, // Will be set after the fifth step
+      key: "APPEND_WIKIPEDIA_DATA_TRANSFORM_STATE_DISTRICTS_DATA",
+    },
+    {
+      name: "Transform Districts with OSM",
+      function: transformDistrictsWithOSM,
+      input: null, // Will be set after the fourth and seventh steps
+      key: "APPEND_OSM_DATA_TRANSFORM_STATE_DISTRICTS_DATA",
+    },
+    // SOI district boundaries are not always to the point as per present situation
+    // {
+    //   name: "Transform Districts with SOI Geo",
+    //   function: transformDistrictsWithSOIGeo,
+    //   input: null, // Will be set after the sixth and seventh steps
+    //   key: "APPEND_SOI_DATA_TRANSFORM_STATE_DISTRICTS_DATA",
+    // },
+  ];
+
+  let outputs: Record<string, any> = {
+    stateUT,
+    districtsList,
+    districtsCount: 0,
+    districts: [],
+    state_union_territory_id: "",
+    state_union_territory_osm_id: "",
+    state_osm_data: {},
+    districtsRelationIds: {},
+    osmDistrictsCount: 0,
+    districtsOSMDetails: [],
+    districtsOSMDetailsNotFound: [],
+    osmDetailsDistrictsCount: 0,
+    districtsWikiDetails: [],
+    districtsWikiDetailsFailed: [],
+    districtFeaturesSOI: [],
+    transformedDistrictsWikipedia: [],
+    districtsNotTransformedWikipedia: [],
+    fullMatchDistrictsOSMWiki: [],
+    partialMatchDistrictsOSMWiki: [],
+    allMatchedDistrictsOSMWiki: [],
+    matchDistrictsOSMWikiStatistics: {},
+    unmatchedDistrictsOSMWiki: [],
+    transformedDistrictsSOIGeo: [],
+    unmatchedDistrictsSOIGeo: [],
+  };
+
+  if (saveToKG) {
+    steps.push({
+      name: "Save Districts to KnowledgeGraph",
+      function: addDistrictDataToKnowledgeGraph,
+      input: null,
+      key: "SAVE_DISTRICT_DATA_TO_KNOWLEDGE_GRAPH",
+    });
+  }
+
+  const districtsProgressDir = path.join(__dirname, "../", "district-pipeline-logs");
+  const progressStatusFile = path.join(districtsProgressDir, "progressStatus.json");
+
+  try {
+    const lastStepOutput = await runPipeline(steps, outputs, districtsProgressDir, progressStatusFile);
+
+    if (saveToKG) return lastStepOutput.savedToKnowledgeGraph;
+    return lastStepOutput.transformedDistrictsSOIGeo;
+  } catch (error) {
+    console.error("Error in processing: ", error);
+  }
+}
+
+(async () => {
+  const stateUT = {
+    name: "tripura",
+    name_id: "in-sut-tripura",
+    vehicle_code: "TR",
+  };
+
+  const districtsList = [
+    {
+      names: ["Dhalai"],
+      wikipedia_page: "https://en.wikipedia.org/wiki/Dhalai_district",
+    },
+    {
+      names: ["Gomati"],
+      wikipedia_page: "https://en.wikipedia.org/wiki/Gomati_district",
+    },
+    {
+      names: ["Khowai"],
+      wikipedia_page: "https://en.wikipedia.org/wiki/Khowai_district",
+    },
+    {
+      names: ["North Tripura"],
+      wikipedia_page: "https://en.wikipedia.org/wiki/North_Tripura_district",
+    },
+    {
+      names: ["Sepahijala"],
+      wikipedia_page: "https://en.wikipedia.org/wiki/Sipahijala_district",
+    },
+    {
+      names: ["South Tripura"],
+      wikipedia_page: "https://en.wikipedia.org/wiki/South_Tripura_district",
+    },
+    {
+      names: ["Unakoti"],
+      wikipedia_page: "https://en.wikipedia.org/wiki/Unakoti_district",
+    },
+    {
+      names: ["West Tripura"],
+      wikipedia_page: "https://en.wikipedia.org/wiki/West_Tripura_district",
+    },
+  ];
+
+  const saveToKG = true;
+
+  await districtsPipeline(stateUT, districtsList, saveToKG);
+})();
