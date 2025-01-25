59b03aa65b567fd96b728f5589d303ed9d45895c
ashish rana <dev.livelifelively@gmail.com>
2025-01-25 22:03:46 +0530

    meghalay ls

--- api/js-ts/src/kg-pipelines/d-v-l/meghalay/scripts/districts.ts
+++ api/js-ts/src/kg-pipelines/d-v-l/meghalay/scripts/districts.ts
@@ -126,7 +126,7 @@ async function districtsPipeline(stateUT: any, districtsList: any, saveToKG: boo
 
 (async () => {
   const stateUT = {
-    state: "meghalaya",
+    name: "meghalaya",
     name_id: "in-sut-meghalaya",
     vehicle_code: "ML",
   };
