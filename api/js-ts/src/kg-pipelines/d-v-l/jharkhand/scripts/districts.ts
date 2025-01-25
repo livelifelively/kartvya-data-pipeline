2d8c20c7de3fba99962004ab21d3de770ec9fa27
ashish rana <dev.livelifelively@gmail.com>
2025-01-25 20:49:25 +0530

    jharkhand ls

--- api/js-ts/src/kg-pipelines/d-v-l/jharkhand/scripts/districts.ts
+++ api/js-ts/src/kg-pipelines/d-v-l/jharkhand/scripts/districts.ts
@@ -126,7 +126,7 @@ async function districtsPipeline(stateUT: any, districtsList: any, saveToKG: boo
 
 (async () => {
   const stateUT = {
-    state_name: "jharkhand",
+    name: "jharkhand",
     name_id: "in-sut-jharkhand",
     vehicle_code: "JH",
   };
