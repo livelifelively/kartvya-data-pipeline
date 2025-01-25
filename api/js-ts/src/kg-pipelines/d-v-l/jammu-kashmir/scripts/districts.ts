8040db70b15620386e12616c56c9689357e890e4
ashish rana <dev.livelifelively@gmail.com>
2025-01-25 20:39:53 +0530

    jk ls

--- api/js-ts/src/kg-pipelines/d-v-l/jammu-kashmir/scripts/districts.ts
+++ api/js-ts/src/kg-pipelines/d-v-l/jammu-kashmir/scripts/districts.ts
@@ -126,7 +126,7 @@ async function districtsPipeline(stateUT: any, districtsList: any, saveToKG: boo
 
 (async () => {
   const stateUT = {
-    state: "jammu and kashmir",
+    name: "jammu and kashmir",
     name_id: "in-sut-jammu-kashmir",
     vehicle_code: "JK",
   };
