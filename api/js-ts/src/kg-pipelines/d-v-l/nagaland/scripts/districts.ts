f16cd72bbac231ff252581de0418d7cc3fa12f63
ashish rana <dev.livelifelively@gmail.com>
2025-01-25 22:04:10 +0530

    nagaland ls

--- api/js-ts/src/kg-pipelines/d-v-l/nagaland/scripts/districts.ts
+++ api/js-ts/src/kg-pipelines/d-v-l/nagaland/scripts/districts.ts
@@ -126,7 +126,7 @@ async function districtsPipeline(stateUT: any, districtsList: any, saveToKG: boo
 
 (async () => {
   const stateUT = {
-    state: "nagaland",
+    name: "nagaland",
     name_id: "in-sut-nagaland",
     vehicle_code: "NL",
   };
