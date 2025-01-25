3e88bc91fd557339398835b6bea0cc9ec8c0fed6
ashish rana <dev.livelifelively@gmail.com>
2025-01-25 22:03:58 +0530

    mizoram ls

--- api/js-ts/src/kg-pipelines/d-v-l/mizoram/scripts/districts.ts
+++ api/js-ts/src/kg-pipelines/d-v-l/mizoram/scripts/districts.ts
@@ -126,7 +126,7 @@ async function districtsPipeline(stateUT: any, districtsList: any, saveToKG: boo
 
 (async () => {
   const stateUT = {
-    state: "mizoram",
+    name: "mizoram",
     name_id: "in-sut-mizoram",
     vehicle_code: "MZ",
   };
