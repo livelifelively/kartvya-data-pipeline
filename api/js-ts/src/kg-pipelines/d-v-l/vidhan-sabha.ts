b939ba081bb4995168e4ba24ff05a2d299538f61
ashish rana <dev.livelifelively@gmail.com>
2025-01-26 21:39:40 +0530

    vidhansabha flow

---
+++ api/js-ts/src/kg-pipelines/d-v-l/vidhan-sabha.ts
@@ -0,0 +1,110 @@
+import { keyBy, groupBy, forEach } from "lodash";
+
+import fs from "fs";
+import path from "path";
+
+import vidhansabhaSeats from "./vidhansabha-seats.json";
+import { PipelineStep, runPipeline } from "../../pipeline/pipeline";
+import {
+  addVidhansabhaConstituencyDataToKnowledgeGraph,
+  checkForDuplicateNameIds,
+  fetchVidhansabhaConstituenciesWikiDetails,
+  fetchVidhansabhaConstituencyECIGeoFeatures,
+  transformVidhansabhaConstituenciesWikipediaData,
+  transformVidhansabhaConstituenciesWithECIGeo,
+} from "../../pipeline/vidhansabha-pipeline";
+
+async function vidhansabhaConstituenciesPipeline(
+  stateUT: any,
+  vidhansabhaConstituenciesList: any,
+  saveToKG: boolean = false
+) {
+  console.log("VIDHANSABHA PROCESSING INITIALIZED: ", stateUT.name);
+
+  const steps: PipelineStep[] = [
+    // {
+    //   name: "Fetch State Vidhansabha_Constituency",
+    //   function: fetchStateVidhansabhaConstituencies,
+    //   key: "STATE_VIDHANSABHA_LIST",
+    //   input: stateUT,
+    // },
+    {
+      name: "Fetch Vidhansabha_Constituency Wiki Details",
+      function: fetchVidhansabhaConstituenciesWikiDetails,
+      key: "STATE_VIDHANSABHA_CONSTITUENCY_WIKI_DATA",
+      input: null, // Will be set after the first step
+    },
+    {
+      name: "Fetch VidhansabhaConstituency ECI Geo Features",
+      function: fetchVidhansabhaConstituencyECIGeoFeatures,
+      key: "STATE_VIDHANSABHA_CONSTITUENCY_ECI_GEO_DATA",
+      input: stateUT,
+    },
+    {
+      name: "Append Wikipedia Data",
+      function: transformVidhansabhaConstituenciesWikipediaData,
+      input: null, // Will be set after the fifth step
+      key: "APPEND_WIKIPEDIA_DATA_TRANSFORM_STATE_VIDHANSABHA_CONSTITUENCY_DATA",
+    },
+    {
+      name: "FIND AND RESOLVE DUPLICATE NAME IDS",
+      function: checkForDuplicateNameIds,
+      input: null,
+      key: "FIND_AND_RESOLVE_DUPLICATE_NAME_IDS_TRANSFORM_STATE_VIDHANSABHA_CONSTITUENCY_DATA",
+    },
+    {
+      name: "Transform Vidhansabha_Constituency with ECI Geo",
+      function: transformVidhansabhaConstituenciesWithECIGeo,
+      input: null, // Will be set after the sixth and seventh steps
+      key: "APPEND_ECI_DATA_TRANSFORM_STATE_VIDHANSABHA_CONSTITUENCY_DATA",
+    },
+  ];
+
+  let outputs: Record<string, any> = {
+    stateUT,
+    vidhansabhaConstituenciesList,
+    stateVidhansabhaConstituencies: vidhansabhaConstituenciesList,
+    vidhansabhaConstituenciesCount: 0,
+    vidhansabhaConstituenciesWikiDetails: [],
+    vidhansabhaConstituenciesWikiDetailsFailed: [],
+    vidhansabhaConstituencyFeaturesECI: [],
+    transformedVidhansabhaConstituenciesWikipedia: [],
+    vidhansabhaConstituenciesNotTransformedWikipedia: [],
+    transformedVidhansabhaConstituenciesECIGeo: [],
+    unmatchedVidhansabhaConstituenciesECIGeo: [],
+    savedToKnowledgeGraph: [],
+  };
+
+  if (saveToKG) {
+    steps.push({
+      name: "Save Vidhansabha_Constituency to KnowledgeGraph",
+      function: addVidhansabhaConstituencyDataToKnowledgeGraph,
+      input: null,
+      key: "SAVE_DISTRICT_DATA_TO_KNOWLEDGE_GRAPH",
+    });
+  }
+
+  const vidhansabhaConstituenciesProgressDir = path.join(
+    __dirname,
+    stateUT.name_id.split("in-sut-")[1],
+    "vidhansabha-constituency-pipeline-logs"
+  );
+
+  const progressStatusFile = path.join(vidhansabhaConstituenciesProgressDir, "progressStatus.json");
+
+  try {
+    const lastStepOutput = await runPipeline(steps, outputs, vidhansabhaConstituenciesProgressDir, progressStatusFile);
+
+    if (saveToKG) return lastStepOutput.savedToKnowledgeGraph;
+    return lastStepOutput.transformedVidhansabhaConstituenciesECIGeo;
+  } catch (error) {
+    console.error("Error in processing: ", error);
+  }
+}
+
+(async () => {
+  let state = vidhansabhaSeats[0];
+  const saveToKG = true;
+
+  await vidhansabhaConstituenciesPipeline(state, state.vidhansabha_constituencies, saveToKG);
+})();
